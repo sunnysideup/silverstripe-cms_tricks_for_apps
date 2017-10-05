3ed936bb9dbe6d6d72dd6b5707d63c34ba24f85b
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2017-10-05 16:31:50 +1300

    PATCH: fixes

--- samples/MyDataObject.php
+++ samples/MyDataObject.php
@@ -2,133 +2,134 @@
 
 class MyDataObject extends DataObject {
 
-	private static $db = array(
-		'Code' => 'Int',
-		'Title' => 'Varchar(255)',
-		'NumericalValue' => 'Int'
-		'Required' => 'Boolean'
-	);
+    private static $db = array(
+        'Code' => 'Int',
+        'Title' => 'Varchar(255)',
+        'NumericalValue' => 'Int'
+        'Required' => 'Boolean'
+    );
 
-	private static $has_one = array(
-		"MyHasOne" => "MyHasOne",
-		"RequiredNice" => "MyHasOne"
-	);
+    private static $has_one = array(
+        "MyHasOne" => "MyHasOne",
+        "RequiredNice" => "MyHasOne"
+    );
 
-	private static $has_many = array(
-	);
+    private static $has_many = array(
+    );
 
-	private static $many_many = array(
-		'MyParentDataObjects' => 'MyParentDataObject'
-	);
+    private static $many_many = array(
+        'MyParentDataObjects' => 'MyParentDataObject'
+    );
 
-	private static $belongs_many_many = array(
-	);
+    private static $belongs_many_many = array(
+    );
 
-	private static $casting = array(
-		"RequiredNice" => "Varchar"
-	);
+    private static $casting = array(
+        "RequiredNice" => "Varchar"
+    );
 
-	private static $indexes = array(
-		"FullTitle" => "Varchar"
-	);
+    private static $indexes = array(
+        "FullTitle" => true,
+        'NumericalValue' => true
+    );
 
-	private static $default_sort = 'MyDataObject.NumericalValue ASC';
+    private static $default_sort = 'MyDataObject.NumericalValue ASC';
 
-	private static $required_fields = array();
+    private static $required_fields = array();
 
-	private static $summary_fields = array(
-		'FullTitle' => 'Title (Code: Title)',
-		'RequiredNice' => 'Is this Required'
-	);
+    private static $summary_fields = array(
+        'FullTitle' => 'Title (Code: Title)',
+        'RequiredNice' => 'Is this Required'
+    );
 
-	private static $field_labels = array(
-	);
+    private static $field_labels = array(
+    );
 
-	private static $searchable_fields = array(
-	);
+    private static $searchable_fields = array(
+    );
 
-	private static $singular_name = "My Data Object";
+    private static $singular_name = "My Data Object";
 
-	private static $plural_name = "My Data Objects";
+    private static $plural_name = "My Data Objects";
 
-	function CMSEditLink() {
-		$controller = singleton("SampleModelAdmin");
-		return $controller->Link()."MyDataObject/EditForm/field/MyDataObject/item/".$this->ID."/edit";
-	}
+    function CMSEditLink() {
+        $controller = singleton("SampleModelAdmin");
+        return $controller->Link()."MyDataObject/EditForm/field/MyDataObject/item/".$this->ID."/edit";
+    }
 
-	function getCMSFields() {
-		$fieldLabels = $this->config()->get('field_labels');
-		if($this->exists()) {
-			$field = $fields->findOrMakeTab('Root.MyParentDataObjects')->fieldByName('MyParentDataObjects');
-			$field->getConfig()
-				->removeComponentsByType('GridFieldAddExistingAutocompleter')
-				->removeComponentsByType('GridFieldAddNewButton');
-		}
-		CMSTricks::add_links_to_fields($this, $fields);
-		return $fields;
-	}
+    function getCMSFields() {
+        $fieldLabels = $this->config()->get('field_labels');
+        if($this->exists()) {
+            $field = $fields->findOrMakeTab('Root.MyParentDataObjects')->fieldByName('MyParentDataObjects');
+            $field->getConfig()
+                ->removeComponentsByType('GridFieldAddExistingAutocompleter')
+                ->removeComponentsByType('GridFieldAddNewButton');
+        }
+        CMSTricks::add_links_to_fields($this, $fields);
+        return $fields;
+    }
 
-	/**
-	 * returns list of fields as they are exported
-	 * @return array
-	 * Field => Label
-	 */
-	function getExportFields(){
-		return array(
-			'MyHasOneID' => 'MyHasOne  ID',
-			'MyHasOne.Title' => 'MyHasOne Title',
-			'ID' => 'ID',
-			'Title' => 'Title'
-		);
-	}
+    /**
+     * returns list of fields as they are exported
+     * @return array
+     * Field => Label
+     */
+    function getExportFields(){
+        return array(
+            'MyHasOneID' => 'MyHasOne  ID',
+            'MyHasOne.Title' => 'MyHasOne Title',
+            'ID' => 'ID',
+            'Title' => 'Title'
+        );
+    }
 
-	function FullTitle() {return $this->getFullTitle();}
-	function getFullTitle() {
-		return $this->Code.": ".$this->Title;
-	}
-
-
-	function canCreate($member = null) {
-		return true;
-	}
-
-	function canView($member = null) {
-		return true;
-	}
-
-	function canEdit($member = null) {
-		return ! $this->MyParentDataObjects()->count();
-	}
+    function FullTitle() {return $this->getFullTitle();}
+    function getFullTitle() {
+        return $this->Code.": ".$this->Title;
+    }
 
 
-	function canDelete($member = null) {
-		return false;
-	}
+    function canCreate($member = null) {
+        return true;
+    }
 
-	//function RequiredNice(){return $this->getRequiredNice();}
-	function getRequiredNice(){
-		return $this->dbObject('Required')->Nice();
-	}
+    function canView($member = null) {
+        return true;
+    }
+
+    function canEdit($member = null) {
+        return ! $this->MyParentDataObjects()->count();
+    }
 
 
-	public function requireDefaultRecords(){
-		parent::requireDefaultRecords();
-	}
+    function canDelete($member = null) {
+        return false;
+    }
 
-	public function populateDefaults() {
-		parent::populateDefaults();
-		if(!$this->NumericalValue) {
-			$this->NumericalValue = 999999;
-		}
-	}
+    //function RequiredNice(){return $this->getRequiredNice();}
+    function getRequiredNice(){
+        return $this->dbObject('Required')->Nice();
+    }
 
-	function onBeforeWrite(){
-		parent::onBeforeWrite();
-	}
 
-	function onAfterWrite(){
-		parent::onAfterWrite();
-	}
+    public function requireDefaultRecords(){
+        parent::requireDefaultRecords();
+    }
+
+    public function populateDefaults() {
+        parent::populateDefaults();
+        if(!$this->NumericalValue) {
+            $this->NumericalValue = 999999;
+        }
+    }
+
+    function onBeforeWrite(){
+        parent::onBeforeWrite();
+    }
+
+    function onAfterWrite(){
+        parent::onAfterWrite();
+    }
 
 
 }
